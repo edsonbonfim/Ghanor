9c6065b8f9c506533e473db90a8a58fd6d914ff0
Edson Onildo <inbox.edsononildo@gmail.com>
2018-05-17 14:32:06 -0300

    Adicionando arquivos que estao faltando (ainda incompleto)

---
+++ cmake-build-debug/CMakeFiles/Ghanor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ghanor.dir/main.c.o"
+  "Ghanor.pdb"
+  "Ghanor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ghanor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
